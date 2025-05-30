ee6f62e1d56000e1fb649ed31d4e1075f8d4cf86
dyahtri <dyahttari@gmail.com>
2025-05-30 13:07:38 +0700

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -179,7 +179,7 @@ elif page == "🔢 ABC Analysis":
 
         abc_summary_display["Jumlah Nilai Rupiah"] = abc_summary_display["Jumlah Nilai Rupiah"].apply(lambda x: f"Rp {x:,.0f}".replace(",", "."))
         
-        abc_summary["Persentase Jumlah Kuantitas"] = abc_summary_display["Persentase Jumlah Kuantitas"].map("{:.2f}%".format)
+        abc_summary_display["Persentase Jumlah Kuantitas"] = abc_summary_display["Persentase Jumlah Kuantitas"].map("{:.2f}%".format)
         abc_summary_display["Persentase Penyerapan Nilai Rupiah"] = abc_summary_display["Persentase Penyerapan Nilai Rupiah"].map("{:.2f}%".format)
 
         st.table(abc_summary_display)
